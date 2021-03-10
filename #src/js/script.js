4ecbd4a1f48b7bec04620d755ba4772904c51940
frontEndDevel <front_end_dev@yahoo.com>
2021-03-10 21:37:13 +0200

    section services hover effect done

--- #src/js/script.js
+++ #src/js/script.js
@@ -5,4 +5,5 @@
 	@@include("scripts/components/_parallax.js");
 	@@include("scripts/components/_header.js");
 	@@include("scripts/components/_menu.js");
+	@@include("scripts/components/_hover_overlay.js");
 })();
--- zinc/js/script.js
+++ zinc/js/script.js
@@ -946,4 +946,23 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
     $dropdownrMenu.toggleClass('opened');
   });
   ;
+  $('.corporate').on('mouseenter', function () {
+    $('.overlay__corporate').addClass('show');
+  });
+  $('.corporate').on('mouseout', function () {
+    $('.overlay__corporate').removeClass('show');
+  });
+  $('.wedding').on('mouseenter', function () {
+    $('.overlay__wedding').addClass('show');
+  });
+  $('.wedding').on('mouseout', function () {
+    $('.overlay__wedding').removeClass('show');
+  });
+  $('.private').on('mouseenter', function () {
+    $('.overlay__private').addClass('show');
+  });
+  $('.private').on('mouseout', function () {
+    $('.overlay__private').removeClass('show');
+  });
+  ;
 })();
