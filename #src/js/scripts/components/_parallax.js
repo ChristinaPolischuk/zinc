c7955c3ca6ca09b46f99b8aaf11fc3fd9c55d2a2
artemsaribekyan <asara4671@gmail.com>
2021-03-10 17:27:49 +0200

    finished two sections on corporate page almost finished fusion section on home page

--- #src/js/scripts/components/_parallax.js
+++ #src/js/scripts/components/_parallax.js
@@ -18,7 +18,7 @@
 // 		});
 // 	}
 
-// 	// the first image should be positioned against the top. Use px on the animating part to work with GSAP. 
+// 	// the first image should be positioned against the top. Use px on the animating part to work with GSAP.
 // 	else {
 // 		section.bg.style.backgroundPosition = "50% 0px";
 
@@ -34,3 +34,84 @@
 // 		});
 // 	}
 // });
+
+// $(window).scroll(function() {
+//     let scrollTop = $(this).scrollTop();
+//     let blockTop = $('.fading-out-block').offset().top;
+//     if (scrollTop > blockTop) {
+//         console.log('change opac');
+//         $('.fading-out-block').css({
+//             opacity: function() {
+//                 var elementHeight = $(this).height();
+//                 console.log(elementHeight);
+//                 // return ((elementHeight - scrollTop) / elementHeight);
+//             }
+//         });
+//     }
+// });
+// $(document).ready(function () {
+//     const stickyElm = document.querySelector('.fading-out-block');
+//     // get the first parent element which is scrollable
+//     const stickyElmScrollableParent = getScrollParent(stickyElm);
+//
+//     // save the original offsetTop. when this changes, it means stickiness has begun.
+//     stickyElm._originalOffsetTop = stickyElm.offsetTop;
+//
+//     let lastTop = 0;
+//     // compare previous scrollTop to current one
+//     const detectStickiness = (elm, cb) => () => {
+//         // console.log(lastTop, elm.offsetTop); // from 0 to 407
+//         // console.log(elm._originalOffsetTop); // 0 or 407
+//         // console.log('_maxScrollOffsetTop', elm._maxScrollOffsetTop);
+//         // if (elm._originalOffsetTop <= 0) {
+//         //     console.log('above block');
+//         // } else if (elm._originalOffsetTop > 0) {
+//         //     console.log('under block');
+//         // }
+//         let result = cb && cb(lastTop !== elm.offsetTop);
+//         lastTop = elm.offsetTop;
+//         return cb && cb(elm.offsetTop !== elm._originalOffsetTop);
+//     };
+//
+//     // Act if sticky or not
+//     const onSticky = isSticky => {
+//         stickyElm.classList.toggle('isSticky', isSticky)
+//     };
+//
+//     const scrollCallback = throttle(detectStickiness(stickyElm, onSticky), 100);
+//     stickyElmScrollableParent.addEventListener('scroll', scrollCallback);
+//
+//     // find-first-scrollable-parent
+//     function getScrollParent(element, includeHidden) {
+//         var style = getComputedStyle(element),
+//             excludeStaticParent = style.position === "absolute",
+//             overflowRegex = includeHidden ? /(auto|scroll|hidden)/ : /(auto|scroll)/;
+//
+//         if (style.position !== "fixed")
+//             for (var parent = element; (parent = parent.parentElement); ){
+//                 style = getComputedStyle(parent);
+//                 if (excludeStaticParent && style.position === "static")
+//                     continue;
+//                 if (overflowRegex.test(style.overflow + style.overflowY + style.overflowX))
+//                     return parent;
+//             }
+//
+//         return window;
+//     }
+//
+//     // Throttle
+//     function throttle (callback, limit) {
+//         var wait = false;
+//         return function () {
+//             if (!wait) {
+//                 callback.call();
+//                 wait = true;
+//                 setTimeout(function () {
+//                     wait = false;
+//                 }, limit);
+//             }
+//         }
+//     }
+// });
+//
+
